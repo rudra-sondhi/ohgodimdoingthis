424b9c01b998ef27b151e0a28be650c36b2f80e9
rudra-sondhi <78965901+rudra-sondhi@users.noreply.github.com>
2022-01-15 18:28:08 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function Blind(){
+    document.getElementById("p1").innerHTML = "Blindly in love with you ahahhaIAMahaha";
+}
+
+function yes(){
+    document.getElementById("p1").innerHTML = "Yes, who else could fuck this up LOL"; 
+}
+
+function hopefully(){
+    
+    document.getElementById("p1").innerHTML = "No, that's all you."; 
+}
